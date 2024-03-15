c93bbb6d114b4f2901f7f2fc8a62a2d445dd6679
Madhur Dixit <dmadhur6@gmail.com>
2024-03-15 10:58:03 +0400

    Basic PWA setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+    return (
+        <>
+            <p>Morpheus PWA</p>
+        </>
+    )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+            manifest: {
+                name: 'Morpheus PWA Project',
+                short_name: 'Morpheus PWA Project',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'pwa-64x64.png',
+                        sizes: '64x64',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any'
+                    },
+                    {
+                        src: 'maskable-icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    }
+                ],
+            },
+        })
+
+    ],
+})
